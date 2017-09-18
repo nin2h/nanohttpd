cf68e61589e9336fa36e99d8e3a4b1b810bfa7e4
nino <nino@twohead.org>
2017-09-18 15:17:58 +0200

    add websocket

--- Android.mk
+++ Android.mk
@@ -2,7 +2,8 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := \
-    $(call all-java-files-under, core/src/main/java)
+    $(call all-java-files-under, core/src/main/java) \
+    $(call all-java-files-under, websocket/src/main/java)
 
 
 LOCAL_MODULE := nanohttpd
